a0cc68e9a890d1a67d220d829864805bc549c65a
olivarmbrito <olivarbrito@forgescom.pt>
2014-09-30 18:25:07 +0100

    Shark move.

---
+++ Assets/Working Folder/Olivar/mini-jogo-acelerometro/Scripts/SharkFin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SharkFin : MonoBehaviour {
+	void start()
+	{
+	}
+
+	void Update()
+	{
+
+		Transform target;
+		float speed = 5f;
+
+		target = GameObject.FindWithTag ("Player").transform;
+		Vector3 targetHeading = target.position - transform.position;
+		Vector3 targetDirection = targetHeading.normalized;
+		
+		//rotate to look at the player
+		transform.rotation = Quaternion.LookRotation(targetDirection); // Converts target direction vector to Quaternion
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 50f, 0);
+
+//		transform.LookAt(targetDirection);
+//		transform.Rotate(new Vector3(0,90,0), Space.Self);//correcting the original rotation
+
+		//move towards the player
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}	
+}
