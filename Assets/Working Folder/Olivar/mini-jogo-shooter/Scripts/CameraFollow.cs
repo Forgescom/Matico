4a1e03625fcebc021e010e692413b02d0565d6ce
olivarmbrito <olivarbrito@forgescom.pt>
2014-11-06 10:58:00 +0000

    Correcções no script da câmara.

--- Assets/Working Folder/Olivar/mini-jogo-shooter/Scripts/CameraFollow.cs
+++ Assets/Working Folder/Olivar/mini-jogo-shooter/Scripts/CameraFollow.cs
@@ -6,9 +6,13 @@ public class CameraFollow : MonoBehaviour
 	[HideInInspector]
 	public Vector3 StartingPosition;
 	
-	private const float minCameraX = 0f;
-	private const float maxCameraX = 18.5f;
+//	private const float minCameraX = 0f;
+//	private const float maxCameraX = 18.5f;
 
+	float MINSCALE = 5.4F; 
+	float MAXSCALE = 7.4F; 
+	float MAX_X_ZOOMIN = 18.5f;
+	float MAX_X_ZOOMOUT = 15;
 
 	[HideInInspector]
 	public bool IsFollowing;
@@ -28,8 +32,14 @@ public class CameraFollow : MonoBehaviour
 		{
 			if (PandaToFollow != null) //panda will be destroyed if it goes out of the scene
 			{
+				float currentSize = transform.camera.orthographicSize;
+				float factor = (MAX_X_ZOOMIN - MAX_X_ZOOMOUT)/(MAXSCALE-MINSCALE);
+				
+				float xMin = 0 +(currentSize - MINSCALE)*factor;
+				float xMax = MAX_X_ZOOMIN -(currentSize-MINSCALE)*factor;
+
 				var pandaPosition = PandaToFollow.transform.position;
-				float x = Mathf.Clamp(pandaPosition.x, minCameraX, maxCameraX);
+				float x = Mathf.Clamp(pandaPosition.x, xMin, xMax);
 
 				//camera follows panda's x position
 				transform.position = new Vector3(x, StartingPosition.y, StartingPosition.z);
@@ -37,7 +47,6 @@ public class CameraFollow : MonoBehaviour
 			else
 				IsFollowing = false;
 		}
-		transform.SendMessage("ClampCameraMovement");
 	}
 
 }
