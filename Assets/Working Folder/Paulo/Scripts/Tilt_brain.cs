8ec2735cfa6c2dcb5823805e6c788edd0fad8db0
Paulo <paulooliveira@forgescom.pt>
2014-12-04 11:39:03 +0000

    Tilt - Tempo com imagem coala

--- Assets/Working Folder/Paulo/Scripts/Tilt_brain.cs
+++ Assets/Working Folder/Paulo/Scripts/Tilt_brain.cs
@@ -25,25 +25,28 @@ public class Tilt_brain : MonoBehaviour {
 
 	//win
 	public string winText;
-
+	//pausa
 	public bool paused;
-
 	//explicar jogo no inicio
 	public GameObject Intro;
+	//display tempo
+	public CntTimeScr timeValue;
 
 	void Start(){
 
-		Intro.GetComponent<Animator>().animation.Play();
+		//Intro.GetComponent<Animator>().animation.Play();
 
 		gameOver = false;
 		restart = false;
 		RestartText.text = "";
 		GameOverText.text = "";
+		timeValue.time = cntTime;
 	}
 
 	//void OnGUI() { GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), introTutorial); }
 
 	void Update(){
+		//Barras progresso 
 		countMaior.barDisplay = Maior;
 		countIgual.barDisplay = Igual;
 		countMenor.barDisplay = Menor;
@@ -150,6 +153,6 @@ public class Tilt_brain : MonoBehaviour {
 	void OnGUI() {
 		//GUI.DrawAnimation(new Rect(0, 0, Screen.width, Screen.height), introTutorial);
 
-		GUI.Box (new Rect(timePos.x, timePos.y, timeSize.x, timeSize.y), "" + cntTime.ToString("0"));
+		//GUI.Box (new Rect(timePos.x, timePos.y, timeSize.x, timeSize.y), "" + cntTime.ToString("0"));
 	} 
 }
