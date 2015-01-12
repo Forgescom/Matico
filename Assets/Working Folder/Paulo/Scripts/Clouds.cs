5d773120dcb98285f0e60713816077d314bc49c0
Paulo <paulooliveira@forgescom.pt>
2015-01-12 14:09:00 +0000

    beta tilt - teste

--- Assets/Working Folder/Paulo/Scripts/Clouds.cs
+++ Assets/Working Folder/Paulo/Scripts/Clouds.cs
@@ -10,15 +10,17 @@ public class Clouds : MonoBehaviour {
 	public float speed = 1f;
 	public bool Horizontal;
 	public bool Rain;
-	public float timeToStart = 2f;
+	//public float timeToStart = 2f;
 
 	private float posx;
-	/*
+
 	void Start () {
-		StartCoroutine(waitToStart());
-	}*/	
+		finalPos = new Vector3 (Random.Range (-10f, 10f), -15f, 0);
+		//StartCoroutine(waitToStart());
+	}
 	// Update is called once per frame
 	void Update () {
+
 		if (canMove) {
 			Move();
 		}
@@ -32,33 +34,33 @@ public class Clouds : MonoBehaviour {
 
 		if (Vector3.Distance (transform.position, finalPos) <= 0) {
 
-			if (Horizontal) {
-				posx = Random.Range (-10f, 10f);
-				initialPos = new Vector3 (posx, Random.Range (12f, 45f), 0);
-				finalPos = new Vector3 (posx, -15f, 0);
+			/**if (Horizontal) {
+				//posx = Random.Range (-10f, 10f);
+				initialPos = new Vector3 (posx, Random.Range (12f, 25f), 0);
+				//finalPos = new Vector3 (posx, -15f, 0);
 			} else if (Rain) {
 				initialPos = new Vector3 (0, Random.Range (40f, 50f), 0);
 				finalPos = new Vector3 (0, -15f, 0);
 			}else{
-				initialPos = new Vector3 (Random.Range (-10f, 10f), Random.Range (12f, 45f), 0);
-				finalPos = new Vector3 (Random.Range (-10f, 10f), -15f, 0);
-			}
-
+				initialPos = new Vector3 (Random.Range (-10f, 10f), Random.Range (12f, 25f), 0);
+				//finalPos = new Vector3 (Random.Range (-10f, 10f), -15f, 0);
+			}**/
+			initialPos = new Vector3 (Random.Range (-10f, 10f), Random.Range (12f, 25f), 0);
 			transform.position = initialPos;
 		}
 	}
 
-	void OnTriggerStay2D(Collider2D col){
+	/*void OnTriggerStay2D(Collider2D col){
 		if (col.name == "eatingKfood") {
 				Restart ();//restart
 				//transform.position = initialPos;
 				}
 		//transform.position = initialPos;
-	}
+	}*/
 
 	//IEnumerator waitToStart() {
 
-	void Restart (){
+	/*void Restart (){
 		//yield return new WaitForSeconds (timeToStart);
 
 		if (Horizontal) {
@@ -73,6 +75,6 @@ public class Clouds : MonoBehaviour {
 			finalPos = new Vector3 (Random.Range (-10f, 10f), -15f, 0);
 		}
 		transform.position = initialPos;
-	}
+	}*/
 
 }
